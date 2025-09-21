9d29d9c5854c909d3d75022e1bbae5bf5dcfdb01
Daniilka48 <WellDevil48@yandex.ru>
2025-09-21 17:15:04 +0300

    fix: page in rest client again

--- src/app/rest-client/[...params]/page.tsx
+++ src/app/rest-client/[...params]/page.tsx
@@ -1,10 +1,10 @@
 import ClientWrapper from '../ClientWrapper';
 
 interface PageProps {
-  params?: { params?: string[] };
+  params: { params: string[] };
 }
 
 export default function RestClientDynamicPage({ params }: PageProps) {
-  const routeParams = params?.params || [];
+  const routeParams = params.params;
   return <ClientWrapper routeParams={routeParams} />;
 }
